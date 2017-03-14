3ff1238a76fdd1a84ff14642fab884d6a23d1b2c
VAELIA1 <VAELIA1@10.131.100.87>
2017-03-14 09:36:34 +0100

    Rental property

--- com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
@@ -13,10 +13,12 @@ import org.eclipse.swt.widgets.Label;
 
 import com.opcoach.training.rental.Rental;
 import com.sii.rental.core.RentalCoreActivator;
+import org.eclipse.swt.widgets.DateTime;
 
 public class RentalPart {
 
-	private Label rentedObjectLabel, customerLabel, startDateLabel,endDateLabel;
+	private Label rentedObjectLabel, customerLabel, startDateLabel,endDateLabel, startDateEntry, endDateEntry;
+	private Group dateGroup;
 	
 	@PostConstruct
 	public void createUi(Composite parent) {
@@ -26,23 +28,49 @@ public class RentalPart {
 		infoGroup.setText("Information");
 		infoGroup.setLayout(new GridLayout(2, false));
 		
-		rentedObjectLabel = new Label(infoGroup, SWT.BORDER);
+		rentedObjectLabel = new Label(infoGroup, SWT.NONE);
 		GridData gd = new GridData();
 		gd.horizontalSpan = 2;
 		gd.horizontalAlignment = SWT.FILL;
 		rentedObjectLabel.setLayoutData(gd);
 		rentedObjectLabel.setText("toto");
 		
-		customerLabel = new Label(infoGroup, SWT.BORDER);
+		customerLabel = new Label(infoGroup, SWT.NONE);
 		customerLabel.setText("tata");
+		new Label(infoGroup, SWT.NONE);
 		
-		this.setRental(RentalCoreActivator.getAgency().getRentals().get(0));	
+		dateGroup = new Group(parent, SWT.NONE);
+		dateGroup.setText("Date");
+		GridData gd_dateGroup = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		gd_dateGroup.widthHint = 120;
+		dateGroup.setLayoutData(gd_dateGroup);
+		dateGroup.setLayout(new GridLayout(2, false));
+		
+		Label startDate = new Label(dateGroup, SWT.NONE);
+		startDate.setBounds(0, 0, 55, 15);
+		startDate.setText("du :");
+		
+		startDateEntry = new Label(dateGroup, SWT.NONE);
+		startDateEntry.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+		startDateEntry.setBounds(0, 0, 55, 15);
+		
+		Label endDate = new Label(dateGroup, SWT.NONE);
+		endDate.setBounds(0, 0, 55, 15);
+		endDate.setText("au :");
+		
+		endDateEntry = new Label(dateGroup, SWT.NONE);
+		endDateEntry.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1));
+		endDateEntry.setBounds(0, 0, 55, 15);
+		
+		this.setRental(RentalCoreActivator.getAgency().getRentals().get(0));
 	}
 	
 	public void setRental(Rental r)
 	{
 		customerLabel.setText(r.getCustomer().getDisplayName());
 		rentedObjectLabel.setText(r.getRentedObject().getName());
+		startDateEntry.setText(r.getStartDate().toString());
+		endDateEntry.setText(r.getEndDate().toString());
 	}
 
 	@Focus
